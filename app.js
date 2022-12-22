94cf1566f0d1597f0c900e20fb7d25834f48ebd6
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-12-22 11:05:10 +0100

    i finaly diid it it's wooooork

--- app.js
+++ app.js
@@ -8,66 +8,71 @@ const input = document.getElementById("textinput");
 var displaytime = document.getElementById("statstime");
 var displayerror = document.getElementById("statstime");
 
-let lenght = 0
+let startingLenght = 0
 const textContentRender = async () => {
     const response = await fetch(ApiUrl);
     let data = await response.json();
 
     var table = data.content.split(" ").map((value) => {
-        lenght++;
+        startingLenght++;
         return value
     });
     console.log(table)
 
-    let n = 0 //number
+    let n = -1 //number
     var arr = data.content.split(" ").map((value) => {
-        lenght++
+        startingLenght++
         n++
         return "<span id='chars-unit"+n+"'>" + value + "</span>"
     });
-    document.getElementById('test').innerHTML += arr.join(" ");
-    document.getElementById('chars-unit2').style.color = 'red'
+    let actualLenght = startingLenght
 
-    console.log('lenght :', lenght, 'words in the sentence' );
+    document.getElementById('test').innerHTML += arr.join(" ");
+    // document.getElementById('chars-unit2').style.color = 'red'
+
+    console.log('lenght :', startingLenght, 'words in the sentence' );
 
     var index = 0;
     // console.log(table[index])
-    console.log(table[index])
-    document.addEventListener('keyup', event => {
-        if (event.code === 'Space') {
-            // console.log('Space pressed')
-            var word = input.value;
-            var word = word.replace(' ','')
+    console.log('word to type :', table[index])
+    // while(actualLenght < 0){
+        document.addEventListener('keyup', event => {
+            if (event.code === 'Space') {
+                // console.log('Space pressed')
+                var word = input.value;
+                var word = word.replace(' ','')
+                input.value = ""
 
-            // console.log('typeof :', typeof table[index] + ' || ' + 'typeof', typeof word)
-            console.log('word.value :', word, 'index :', index, '> :', table[index])
-            console.log(word === table[index])
-            console.log(word, ' || ', table[index])
-            console.log(`${word.length} || ${table[index].length}`);
-            console.log()
-            console.log(document.getElementById('chars-unit2'))
-            console.log('chars-unit' + index)
+                if(word === "" || input.value === " "){
+                    // console.log('ca marche pas ptn')
+                    return 0
+                }
+                
+                // console.log('typeof :', typeof table[index] + ' || ' + 'typeof', typeof word)
+                console.log('word.value :', word, 'index :', index, '> :', table[index])
+                console.log(word === table[index])
+                console.log(word, ' || ', table[index])
+                console.log(`${word.length} || ${table[index].length}`);
+                console.log()
+                // console.log(document.getElementById('chars-unit2'))
+                // console.log('chars-unit' + index)
 
-            if(word === table[index]){
-                console.log('Right word')
-                console.log('chars-unit'+index)
-                idd='chars-unit' + index
-                console.log('idd =', idd)
-                console.log(document.getElementById(idd))
-                // document.getElementById('chars-unit'+index).style.color = 'green'
-            } else{
-                // document.getElementById('chars-unit'+index).style.color = 'green'
+                if(word === table[index]){
+                    console.log('right input >', 'chars-unit'+index)
+                    document.getElementById('chars-unit'+index).style.color = 'green' 
+                    actualLenght--
+                } else{
+                    console.log('false input')
+                    document.getElementById('chars-unit'+index).style.color = 'red'
+                }
+                console.log('remaining words :', actualLenght)  
+
+                if(word !== " "){
+                    index++
+                }
             }
-
-
-
-            if(word !== " "){
-                index++
-            }
-            input.value = ""
-
-        }
-    })
+        })
+    // }
 }; textContentRender();
 
 

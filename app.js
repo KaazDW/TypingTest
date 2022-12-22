af9c5bc40c6f834dd11958230d7d94617f815331
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-12-22 09:35:34 +0100

    update algo typingtest

--- app.js
+++ app.js
@@ -8,60 +8,72 @@ const input = document.getElementById("textinput");
 var displaytime = document.getElementById("statstime");
 var displayerror = document.getElementById("statstime");
 
-let lenght = 0;
+let lenght = 0
 const textContentRender = async () => {
     const response = await fetch(ApiUrl);
     let data = await response.json();
 
-    // var arr = data.content.split(" ").map((value) => {
-    //     lenght++
-    //     return value
-    // });
-    // console.log(lenght)
-    // var table
-    // for(let i = 0; i < lenght; i++ ){
-    //     table[i] = "<span class='chars-unit'>" + arr[i] + "</span>"
-    //     console.log(i)
-    // }
-    // console.log(table)
-
-    // let arr = data.content.split("").map((value) => {
     var table = data.content.split(" ").map((value) => {
         lenght++;
         return value
     });
     console.log(table)
-    document.getElementById('test').innerHTML += table.join(" ");
 
-
+    let n = 0 //number
     var arr = data.content.split(" ").map((value) => {
-        lenght++;
-        return "<span class='chars-unit'>" + value + "</span>"
+        lenght++
+        n++
+        return "<span id='chars-unit"+n+"'>" + value + "</span>"
     });
-    // console.log(arr)
-    // console.log(arr[1])
+    document.getElementById('test').innerHTML += arr.join(" ");
+    document.getElementById('chars-unit2').style.color = 'red'
 
-    // word.innerHTML += arr.join("");
-    // word.innerHTML += arr.join(" ");
     console.log('lenght :', lenght, 'words in the sentence' );
-};
+
+    var index = 0;
+    // console.log(table[index])
+    console.log(table[index])
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+            // console.log('Space pressed')
+            var word = input.value;
+            var word = word.replace(' ','')
+
+            // console.log('typeof :', typeof table[index] + ' || ' + 'typeof', typeof word)
+            console.log('word.value :', word, 'index :', index, '> :', table[index])
+            console.log(word === table[index])
+            console.log(word, ' || ', table[index])
+            console.log(`${word.length} || ${table[index].length}`);
+            console.log()
+            console.log(document.getElementById('chars-unit2'))
+            console.log('chars-unit' + index)
+
+            if(word === table[index]){
+                console.log('Right word')
+                console.log('chars-unit'+index)
+                idd='chars-unit' + index
+                console.log('idd =', idd)
+                console.log(document.getElementById(idd))
+                // document.getElementById('chars-unit'+index).style.color = 'green'
+            } else{
+                // document.getElementById('chars-unit'+index).style.color = 'green'
+            }
+
+
+
+            if(word !== " "){
+                index++
+            }
+            input.value = ""
+
+        }
+    })
+}; textContentRender();
+
+
+
+
 // var index = 0;
 // input.addEventListener('input', () => { 
 //     document.getElementById("tmpp").innerHTML= "start";
 // });
- 
-textContentRender();
-
-var index = 0;
-document.addEventListener('keyup', event => {
-    if (event.code === 'Space') {
-        // console.log('Space pressed')
-        var word = input.value;
-        if(word !== " "){
-            index++
-        }
-        console.log(word, index)
-        input.value = ""
-    }
-})
-

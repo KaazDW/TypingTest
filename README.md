5c6ef3d6784df0046b830c33682253429cb3d3cf
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-02-09 15:26:22 +0100

    start rebuild logic

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# dater
+# Keyspeed Typing Test
  
 ```bash
 npm install
 npm start
+```
+```bash
+sass src/assets/scss/style.scss:src/assets/css/style.css -w
 ```
--- dist/typingTest.js
+++ dist/typingTest.js
@@ -1,1 +1,27 @@
 "use strict";
+let startTime;
+let timerId;
+let pressedKeys = [];
+function keysListener() {
+    document.addEventListener("keydown", event => {
+        const key = event.key;
+        console.log(key);
+    });
+}
+keysListener();
+function startTimerAndRecordKeys() {
+    pressedKeys = [];
+    startTime = Date.now();
+    timerId = setInterval(() => {
+        const elapsedTime = (Date.now() - startTime) / 1000;
+        if (elapsedTime >= 20) {
+            clearInterval(timerId);
+            console.log("Chronomètre terminé!");
+            console.log("Touches appuyées:", pressedKeys);
+        }
+    }, 1000);
+}
+const startButton = document.getElementById("startButton");
+if (startButton) {
+    startButton.addEventListener("click", startTimerAndRecordKeys);
+}

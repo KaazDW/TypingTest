fe2163b7dc30d925740e5e0af54a8734e3ad59fd
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-02-12 16:19:33 +0100

    checkSetting()

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
 
 const word = document.getElementById("textcontent")
-
 const input = document.getElementById("textinput")
 const field = document.getElementById("field")
 
-const radiosLangs = document.querySelectorAll('nav input[type="radio"]');
-
 const finaldiv = document.getElementById("finalResult")
-// var FRtable = ["a", "propos", "au-dessus", "à","travers", "agir", "ajouter", "avoir","peur", "après", "âge", "il", "d'accord", "air ", "tous", "seul", "toujours", "suis", "montant", "un", "et", "en","colère", "autre", "répondre", "tout", "n'importe", "apparaître", "pomme", "sont", "zone", "bras", "armée", "autour", "arriver", "art", "comme", "demander", "voler", "tante", "loin", "bébé", "dos", "mauvais", "sac", "boule", "banque", "base", "bain", "être", "haricot", "ours", "lit", "bière", "avant", "commencer", "cloche", "ci-dessous", "meilleur", "grand", "oiseau", "naissance", "mord", "morsure", "noir", "saignement", "bloc", "sang", "coup", "bleu", "planche", "bateau", "corps", "bouillir", "os", "livre", "frontière", "né", "les deux", "bol", "boîte", "garçon", "branche", "courageux", "pain", "pause", "respirer", "pont", "brillant", "apporter", "frère", "marron", "brosse", "construire", "brûler", "bus", "occupé", "mais", "acheter", "par", "gâteau", "appel", "peut", "casquette", "voiture", "carte", "soin", "porter", "étui", "chat", "attraper", "chaise", "chasse", "pas", "fromage", "enfant", "choix", "cercle", "ville", "classe", "intelligent", "propre", "clair","monter", "horloge", "tissu", "nuage", "près", "café", "manteau", "pièce", "froid", "couleur", "peigne", "commun", "comparer", "venir", "contrôler", "cuisiner", "cool", "cuivre", "maïs", "coin", "correct", "coût", "compte", "couvrir", "crash", "croix", "pleurer", "tasse", "couper", "danser", "sombre", "jour", "mort", "décider", "profond", "cerf", "bureau", "mourir", "sale", "plat", "faire", "chien", "porte", "bas", "dessiner", "rêver", "s'habiller", "boire", "conduire", "déposer", "sec", "canard", "poussière", "devoir", "chaque", "oreille", "tôt", "gagner", "terre", "est", "facile", "manger", "effet ", "oeuf", "huit", "autre", "vide", "fin", "ennemi", "profiter", "entrer", "égal", "pair", "événement", "jamais", "chaque", "exact", "sauf", "s'attendre", "expliquer", "visage", "fait", "échouer", "chute", "faux", "famille", "célèbre ", "loin", "ferme", "rapide", "gros", "faute", "peur", "nourrir", "sentir", "fièvre", "peu", "combat", "remplir", "film", "trouver", "fin", "feu", "premier", "poisson", "fit", "cinq", "fix", "flag", "plat", "float", "floor ", "farine", "mouche", "plier", "nourriture", "fou", "pied","pour", "forcer", "forêt", "oublier", "fourchette", "former", "renard", "quatre", "libre", "geler", "frais", "ami", "de ", "avant", "fruit", "complet", "fun", "drôle", "futur", "jeu", "portail", "obtenir", "cadeau", "donner", "heureux", "verre", "aller", "chèvre", "dieu", "or", "bon", "herbe", "tombe", "grand", "vert", "gris", "groupe", "grandir ", "pistolet", "cheveux", "moitié", "centre", "main", "heureux", "dur", "chapeau", "haine", "avoir", "il", "tête", "entendre", "lourd", "coeur", "bonjour", "aide", "poule", "elle", "ici", "cacher", "haut", "colline", "lui", "son", "coup", "passe-temps", "tenir", "trou", "maison", "espoir", "cheval", "chaud", "hôtel", "maison", "comment", "heure", "dépêchez-vous", "blessé", "je", "glace", "idée", "si", "dans", "dans", "inventer", "fer", "est", "île ", "il", "son", "gelée", "travail", "rejoindre", "jus", "sauter", "juste", "garder", "clé", "tuer", "gentil", "roi", "genou", "couteau", "frapper", "savoir", "dame", "lampe", "terre", "grand", "dernier", "tard", "rire", "paresseux", "conduire", "feuille", "apprendre", "partir", "jambe", "gauche", "prêt", "longueur", "moins", "leçon", "laisser","yeux","porter","feuille","attend","mettre","main","outil","toi","nous","vous","ils","sont","tous","et","toute","chauve","sans","cheveux","rire","oiseau"]
+
+
+// Random frequent Word-List by languages
 var frWordList = ["lui","ce","moins","bien","plus","cent","en","monsieur","devoir","vieux","un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix","elle","votre","mais","voir","comme","pas","pays","coeur","mon","après","te","trois","son","même","plus","entre","tu","alors","ni","encore","noir","mot","mort","y","parler","bon","de","non","ou","passer","son","frère","terre","aussi","mer","vous","fille","en","amour","père","terre","penser","pouvoir","que","beau","air","ami","main","sur","mille","coeur","premier","ciel","trouver","nous","enfin","seul","tenir","amour","tant","se","quand","ton","à","penser","jamais","monde","mot","dont","yeux","none","encore","donc","ami","prendre","mordre","manger","dire","doit","boire","outil","éteint","mari","chercher","ou","depuis","alors","prendre","ville","parce","que","ciel","voix","voir","grand"];
 var enWordList = ["him","this","less","well","more","hundred","in","sir","duty","old","one","two","three","four","five","six","seven","eight","nine","ten","she","your","but","see","as","not","country","heart","my","after","you","three","his","same","more","between","you","then","neither","again","black","word","death","there","speak","good","of","no","or","to pass","its","brother","earth","also","sea","you","daughter","in","love","father","earth","think","power","that","beautiful","air","friend","hand","on","thousand","heart","first","sky","find","we","finally","alone","to hold","love","so much","to be","when","your","to","think","never","world","word","whose","eyes","none","again","so","friend","to take","bite","to eat","to say","must","to drink","tool","off","husband","to look for","or","since","then","to take","city","because","that","sky","voice","see","big"];
 
-var FRtable = [];
+// Default language init to french
+var usedWordList = frWordList;
 
-console.log('table.lenght :', FRtable.length)
+const radiosLangs = document.querySelectorAll('nav input[type="radio"]');
 
 radiosLangs.forEach(radio => {
     radio.addEventListener('change', function() {
-        if (this.checked) {
-            console.log('Selected Language :', this.value);
-            if(this.value === 'fr'){
-                FRtable = frWordList
-            } else if(this.value === 'en') {
-                FRtable = enWordList
-            }
-            reload(50)
-        }
+    console.log("checkSettings", checkSettings());
     });
 });
+
+function checkSettings(){
+    let result = [];
+    radiosLangs.forEach(radio => {
+        if (radio.value === 'fr' && radio.checked) {
+            usedWordList = frWordList;
+            result.push('fr');
+        } else if (radio.value === 'en' && radio.checked) {
+            usedWordList = enWordList;
+            result.push('en');
+        } else if (radio.value === 'twenty' && radio.checked) {
+            result.push('twenty');
+        } else if (radio.value === 'fifty' && radio.checked) {
+            result.push('fifty');
+        }
+    });
+    return result;
+}
 
 finaldiv.style.display = 'none'
 
@@ -70,7 +80,7 @@ function reload(nword){
 
     function randtab(x){
         for(let i = 0; i < x; i++){
-            tab = FRtable[Math.floor(Math.random()*FRtable.length)]
+            tab = usedWordList[Math.floor(Math.random()*usedWordList.length)]
             tablelog.push(tab)
         }
     }
@@ -81,23 +91,23 @@ function reload(nword){
     for(let i = 0; i < tablelog.length; i++){
         tabledisplayed.push("<span id='chars-unit" + i + "'>" + tablelog[i] + "</span>")
     }
-    console.log(tabledisplayed)
-    console.log(tablelog)
+    // console.log(tabledisplayed)
+    // console.log(tablelog)
     document.getElementById('quote').innerHTML += tabledisplayed.join(" ")
-    console.log(tablelog[1])
-    console.log(tabledisplayed[1])
+    // console.log(tablelog[1])
+    // console.log(tabledisplayed[1])
 
 
     startingLenght = tablelog.length
 
     actualLenght = startingLenght
-    console.log('lenght :', startingLenght, 'words in the sentence' )
+    // console.log('lenght :', startingLenght, 'words in the sentence' )
 
     document.getElementById('remaining').innerHTML = actualLenght
 
     // console.log('word to type :', table[index])
-    console.log('actualLenght', actualLenght)
-    console.log(actualLenght > 0)        
+    // console.log('actualLenght', actualLenght)
+    // console.log(actualLenght > 0)
 
     document.getElementById('chars-unit'+(index)).style.color = 'rgb(79, 117, 175)'
 }
@@ -113,7 +123,7 @@ function reload(nword){
                 timerStart = true
             }
 
-            console.log(actualLenght)        
+            // console.log(actualLenght)
             if(actualLenght > 0){
                 var word = input.value
                 var word = word.replace(' ','')
@@ -139,7 +149,7 @@ function reload(nword){
                 }
 
                 actualLenght--
-                console.log('remaining words :', actualLenght)  
+                // console.log('remaining words :', actualLenght)
                 document.getElementById('remaining').innerHTML = actualLenght
 
                 if(word !== " "){
@@ -153,9 +163,9 @@ function reload(nword){
                     field.style.display = 'none'
                     document.getElementById("stats").style.display = 'none'
                     colored = false;
-                    console.log( 'duration', duration)
-                    console.log('rightword', rightword)
-                    console.log('word', startingLenght)
+                    // console.log( 'duration', duration)
+                    // console.log('rightword', rightword)
+                    // console.log('word', startingLenght)
 
                     wpmfull = (rightword * 60) / duration
                     wpm = wpmfull.toFixed(3);
@@ -230,6 +240,5 @@ function callreload(){
 document.addEventListener('keyup', event => {
     if (event.code === 'Equal') {
         callreload()
-        // window.location.reload();
     }
 });
